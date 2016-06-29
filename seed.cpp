7ac178978cfc327dc82e767e254415eadcd2cc4e
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2016-06-29 22:26:25 +0530

    Create seed.cpp

---
+++ seed.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>0)
+{
+	for(int i=0;i<n;i++)
+	{int temp=i;
+	int tot=1;
+		while(temp>0)
+		{   
+			tot=tot*temp%10;
+			temp=temp/10;
+		}
+		if((tot*i)==n)
+		cout<<i<<"\n";
+	}
+}else 
+cout<<"error";
+	return 0;
+}
