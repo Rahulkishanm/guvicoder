7f84bb659d2c05640bfd6299d00f889ab4a8b3eb
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2016-06-30 04:30:52 +0530

    Create hunter2.cpp

---
+++ hunter2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void function(int *arr,int n)
+{
+	 for(int k=0;k<n;k++)
+	 {
+	 	if(k==arr[k])
+	 	{
+	 		cout<<k;
+	 	}
+	 }
+}
+
+
+int main() {
+	int *arr=(int *)malloc(1000);
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	cin>>arr[i];
+	function(arr,n);
+	return 0;
+}
