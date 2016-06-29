31eadc495f4b2a96dd34435085b774761ff648ac
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2016-06-30 05:00:46 +0530

    Create firstreatingele.cpp

---
+++ firstreatingele.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int *a=(int *)malloc(100);
+	int n;
+	cin>>n;
+	int *counter=(int *)malloc(999999);
+	
+	///To get value
+	for(int i=0;i<n;i++)
+	{   
+		cin>>a[i];
+	
+	}
+	//to print value 
+	for(int k=0;k<n;k++)
+	{int flag=0;
+		for(int j=k+1;j<n;j++)
+		{
+			if(a[k]==a[j])
+			{
+			cout<<a[k];flag=1;
+			break;
+			}
+		}
+		if(flag==1)
+	    break;	
+	}
+	return 0;
+}
