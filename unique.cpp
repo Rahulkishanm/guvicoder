a70af36a07a9defef09a3f60063584df9eec2e25
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2016-06-30 04:46:23 +0530

    Create unique.cpp

---
+++ unique.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int *a=(int *)malloc(100);
+	int n;
+	cin>>n;
+	int *counter=(int *)malloc(999999);
+	
+	///To get value
+	for(int i=0;i<n;i++)
+	{   
+		cin>>a[i];
+		if(a[i]>0&&a[i]<999999)
+		counter[a[i]]++;
+		else 
+		{
+		 cout<<"enter value between 0 and 999999";
+		 counter[a[i]]++;
+		}
+	}
+	//to print value 
+	for(int k=0;k<n;k++)
+	{
+		if(counter[a[k]]==1)
+		{
+			cout<<a[k]<<"\n";
+			
+		}
+	}
+	return 0;
+}
