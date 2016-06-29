b8a961853c0a5981bdb61d94fc7dcb56c21e0a04
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2016-06-30 01:47:48 +0530

    Create honoi.cpp

---
+++ honoi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void towerofHanoi(int n, char fromrod, char torod, char auxrod)
+{
+	if (n == 1)
+	{
+		printf("\n Moved disk 1 from rod %c to rod %c", fromrod, torod);
+		return;
+	}
+	towerofHanoi(n-1, fromrod, auxrod, torod);
+	printf("\n Moved disk %d from rod %c to rod %c", n, fromrod, torod);
+	towerofHanoi(n-1, auxrod, torod, fromrod);
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	towerofHanoi(n, 'A', 'C', 'B');
+	return 0;
+}
