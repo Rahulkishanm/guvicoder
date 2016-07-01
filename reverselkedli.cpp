7accda8569660c5c9ee875dddbfefc00cdcdd24e
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2016-07-01 17:15:59 +0530

    Create reverselkedli.cpp

---
+++ reverselkedli.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+struct node{
+	int data;
+	node *next;
+	
+}*head=NULL,*current=NULL,*temp=NULL;
+void insert(int data)
+{
+	struct node *newnode=(node *)malloc(sizeof(node));
+	newnode->data=data;
+	newnode->next=NULL;
+	if(head==NULL)
+	{
+		head=newnode;
+		current=newnode;
+	}
+	else
+	{
+		current->next=newnode;
+		current=newnode;
+	}
+}
+void reverse(node * ptr)
+{
+	
+	if(ptr==NULL)
+	return;
+	reverse(ptr->next);
+	
+	cout<<ptr->data;
+
+
+}
+
+int main(void) {
+  
+	insert(100);
+	insert(90);
+	insert(89);
+	temp=head;
+	struct node *ptr=head;
+	reverse(ptr);
+	
+	return 0;
+}
